49bf20f2b9798e4852b39977a13df90d0eebad56
David Sapiro <dsap48@gmail.com>
2024-04-16 11:35:12 -0700

    added upload form, /photos/upload route, and upload template. functional photo upload to local folder

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, flash
+from forms import ImageUploadForm
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
+app.config['UPLOAD_FOLDER'] = "./uploaded_photos"
+
 
 @app.get("/")
 def homepage():
     """Returns homepage"""
 
-    return render_template("index.html")
+    return render_template("base.html")
+
+
+@app.route("/photos/upload", methods=["GET", "POST"])
+def upload_photo():
+    """Displays image upload form and handles submit"""
+
+    form = ImageUploadForm()
+
+    if form.validate_on_submit():
+        try:
+            image_file = form.image.data
+            filename = secure_filename(image_file.filename)
+            image_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            flash("Image upload successful!")
+            return redirect("/")
+
+        except Exception:
+            flash("Image upload failed!")
+
+    return render_template('upload_form.html', form=form)
---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import FileField, SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileAllowed
+
+class ImageUploadForm(FlaskForm):
+    """Form for uploading an image"""
+
+    image = FileField('Image', validators=[
+        DataRequired(),
+        FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')
+    ])
+
+    submit = SubmitField('Upload')
+
