daf3a95630565ace0788ad5a906ab8537d176138
Cooper Spinelli <cooperspinelli@gmail.com>
2024-04-19 00:19:18 -0600

    Add docstrings and format app

--- app.py
+++ app.py
@@ -12,12 +12,10 @@ from PIL import Image as PillowImage
 import os
 from utils import get_formatted_metadata, get_metadata_for_display
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = "oh-so-secret"
-app.config['UPLOAD_FOLDER'] = "./uploaded_photos"
-
 load_dotenv()
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ["APPLICATION_SECRET_KEY"]
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 app.config['SQLALCHEMY_ECHO'] = False
 
@@ -30,21 +28,42 @@ s3 = boto3.client(
     aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
     region_name=os.getenv("AWS_REGION"),
 )
-bucket_name = os.getenv("AWS_BUCKET_NAME")
 
+
+bucket_name = os.getenv("AWS_BUCKET_NAME")
 base_url = f"https://{bucket_name}.s3.{os.getenv('AWS_REGION')}.amazonaws.com/"
+
 MAPQUEST_API_KEY = os.getenv("MAPQUEST_API_KEY")
+
+############
+## ROUTES ##
+############
+
 
 @app.get("/")
 def homepage():
-    """Returns homepage"""
+    """
+    Render the homepage template.
+
+    Returns:
+        A rendered template 'home.html' for the homepage.
+    """
 
     return render_template("home.html")
 
 
 @app.route("/photos/upload", methods=["GET", "POST"])
 def upload_photo():
-    """Displays image upload form and handles submit"""
+    """
+    Displays an image upload form and handles the image submission.
+    If the form is submitted and validated, the image is uploaded to AWS S3
+    and its metadata is saved in the database.
+
+    Returns:
+        On successful upload: Redirects to the photo viewing page and flashes
+            success message.
+        On failure: Reloads the upload form and flashes an error message.
+    """
 
     form = ImageUploadForm()
 
@@ -103,24 +122,43 @@ def upload_photo():
         finally:
             db.session.commit()
 
-
     return render_template('upload_form.html', form=form)
 
 
 @app.get("/photos")
 def display_photos():
+    """
+    Fetches and displays all photos or filtered photos based on the search
+    term provided in the query parameter.
+
+    Query string args:
+        searchTerm (str): Optional. A string to filter photos based on their metadata.
+
+    Returns:
+        A rendered template 'all_photos.html' displaying all or filtered photos.
+    """
 
     search_term = request.args.get("searchTerm", "")
     if search_term:
-        photos = Image.query.filter(Image.search_vector.ilike(f"%{search_term}%")).all()
+        photos = Image.query.filter(
+            Image.search_vector.ilike(f"%{search_term}%")).all()
     else:
-        photos = Image.query.all() # Return all photos if no search term is provided
+        photos = Image.query.all()  # Return all photos if no search term is provided
 
     return render_template("all_photos.html", photos=photos, base_url=base_url)
 
 
 @app.get("/photos/<photo_name>")
 def display_photo(photo_name):
+    """
+    Display a single photo along with its metadata.
+
+    URL Args:
+        photo_name (str): The filename of the photo to retrieve.
+
+    Returns:
+        A rendered template 'display_photo.html' showing the photo and its metadata.
+    """
 
     photo = Image.query.get_or_404(photo_name)
 
@@ -131,6 +169,15 @@ def display_photo(photo_name):
 
 @app.get("/photos/<photo_name>/edit")
 def edit_photo(photo_name):
+    """
+    Renders an edit page for a specific photo identified by its name.
+
+    URL Args:
+        photo_name (str): The filename of the photo to edit.
+
+    Returns:
+        A rendered template 'edit_photo.html' for editing the photo.
+    """
 
     photo = Image.query.get_or_404(photo_name)
 
@@ -139,34 +186,58 @@ def edit_photo(photo_name):
 
 @app.post("/photos/<photo_name>/edit")
 def upload_edited_photo(photo_name):
+    """
+    Handles the submission of the edited photo. Updates the photo's metadata
+    and re-uploads to AWS S3.
 
-    print(request.files)
+    URL Args:
+        photo_name (str): The filename of the photo being edited.
 
-    photo = Image.query.get(photo_name)
-    image_file = request.files["image"]
+    Returns:
+        JSON response indicating the outcome of the save operation.
+    """
 
-    photo.height = request.form["height"]
-    photo.width = request.form["width"]
+    try:
+        image_file = request.files["image"]
 
-    db.session.commit()
+        photo = Image.query.get(photo_name)
+        photo.height = request.form["height"]
+        photo.width = request.form["width"]
 
-    s3.upload_fileobj(
-        image_file,
-        bucket_name,
-        photo_name
-    )
+        db.session.commit()
 
-    return jsonify({
-        "message": "Image saved"
-    })
+        s3.upload_fileobj(
+            image_file,
+            bucket_name,
+            photo_name
+        )
 
+        return jsonify({
+            "message": "Image saved"
+        })
+
+    except Exception as ex:
+        return jsonify({
+            "message": ex
+        })
 
 
 @app.route('/get_map')
 def get_map():
+    """
+    Generates and returns a static map image from MapQuest based on the
+    provided latitude and longitude.
+
+    Query Parameters:
+        location (str): A string in the format "latitude,longitude"
+            indicating the center of the map.
+
+    Returns:
+        A PNG image file of the map if successful, or an error message if failed.
+    """
+
     [latitude, longitude] = request.args["location"].split(", ")
 
-    # Parameters as a dictionary
     params = {
         'key': MAPQUEST_API_KEY,
         'center': f"{latitude},{longitude}",
@@ -175,20 +246,17 @@ def get_map():
         'locations': f"{latitude},{longitude}|marker-sm-22407F-2A4DB3"
     }
 
-    # Constructing the URL with urlencode
     base_url = "https://www.mapquestapi.com/staticmap/v5/map"
     query_string = urlencode(params)
     map_url = f"{base_url}?{query_string}"
 
     response = requests.get(map_url)
 
-    # Check if the request was successful
     if response.status_code == 200:
-        # Convert the response content into a BytesIO object
+
         image = BytesIO(response.content)
         image.seek(0)
 
-        # Serve the image directly
         return send_file(image, mimetype='image/png')
     else:
         return "Failed to fetch the map", 500
